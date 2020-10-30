9b99af0f00eaedd7d0c5fdcac5850586ece11b78
Miguel-Pariente <miguelpl32@hotmail.com>
2020-10-30 12:20:18 +0100

    modificacion path

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-app.use(express.static(`${__dirname} + /dist/youtuveApp`));
+app.use(express.static(`${__dirname}/dist/youtuveApp`));
 app.get("/*", function (req, res) {
   res.sendFile(path.join(`./dist/youtubeApp/index.html`));
 });
