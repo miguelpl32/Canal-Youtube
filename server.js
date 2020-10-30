fa992c700c9584c3275bca05138fda67339484de
Miguel-Pariente <miguelpl32@hotmail.com>
2020-10-30 11:52:49 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-app.use(express.static(`${__dirname} + /dist/youtuveapp`));
+app.use(express.static(`${__dirname} + /dist/youtuveApp`));
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(`./dist/youtubeapp/index.html`));
+  res.sendFile(path.join(`./dist/youtubeApp/index.html`));
 });
 app.listen(process.env.PORT || 3000);
